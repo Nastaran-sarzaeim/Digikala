e40d496fd7d1c8eff75b45206a5a6d623d7b5707
Applestar <applestar@Applestars-MacBook-Pro.local>
2024-07-30 01:57:07 +0330

    fix url

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://api.digikala.com',
+        target: 'http://localhost:5002',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
